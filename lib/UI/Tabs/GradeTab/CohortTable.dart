664af2c08204255c8be0fa7618e14750d0c802b7
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-06-19 19:34:00 +0200

    Use Listview.builder for Cohort Grid

--- lib/UI/Tabs/GradeTab/CohortTable.dart
+++ lib/UI/Tabs/GradeTab/CohortTable.dart
@@ -15,19 +15,26 @@ class DrawCohortTable extends StatelessWidget {
       }
     }
 
-    return new Center(
-      child: GridView.builder(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: data[0].length),
-        itemCount: _newData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return new GridTile(
-            child: Text(_newData[index]["text"].toString()),
-          );
-        },
-      ),
+    // return new Center(
+    //   child: GridView.builder(
+    //     shrinkWrap: true,
+    //     scrollDirection: Axis.horizontal,
+    //     gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+    //         crossAxisCount: data[0].length),
+    //     itemCount: _newData.length,
+    //     itemBuilder: (BuildContext context, int index) {
+    //       return new GridTile(
+    //         child: Text(_newData[index]["text"].toString()),
+    //       );
+    //     },
+    //   ),
+    // );
+
+    return new ListView.builder(
+      itemCount: data[0].length,
+      itemBuilder: (BuildContext context, int i) => Row(children: <Widget>[
+        new Text(data[0][i]), new Text(data[1][i])
+      ],),
     );
   }
 }
