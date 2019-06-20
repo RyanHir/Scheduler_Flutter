326a85afebe55ad3ec4b2139662e354569d81f7c
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-06-20 17:53:07 +0200

    Begin use of cards in CohortTable

--- lib/UI/Tabs/GradeTab/CohortTable.dart
+++ lib/UI/Tabs/GradeTab/CohortTable.dart
@@ -1,40 +1,36 @@
 import 'package:flutter/material.dart';
 
 class DrawCohortTable extends StatelessWidget {
-  final data;
+  final List<dynamic> data;
 
   DrawCohortTable(this.data);
 
   @override
   Widget build(BuildContext context) {
-    List<dynamic> _newData = [];
-    for (var x in data) {
-      if (x == null) return new Center(child: Text("Please Reload Data"));
-      for (var y in x) {
-        _newData.insert(_newData.length, y);
-      }
-    }
 
-    // return new Center(
-    //   child: GridView.builder(
-    //     shrinkWrap: true,
-    //     scrollDirection: Axis.horizontal,
-    //     gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-    //         crossAxisCount: data[0].length),
-    //     itemCount: _newData.length,
-    //     itemBuilder: (BuildContext context, int index) {
-    //       return new GridTile(
-    //         child: Text(_newData[index]["text"].toString()),
-    //       );
-    //     },
-    //   ),
-    // );
+    List<dynamic> newData = data.toList();
+
+    newData.forEach((obj) => obj.removeWhere(
+      (item) => item["type"] == "QUERY" || item["type"] == "KEY"
+    ));
 
     return new ListView.builder(
-      itemCount: data[0].length,
-      itemBuilder: (BuildContext context, int i) => Row(children: <Widget>[
-        new Text(data[0][i]), new Text(data[1][i])
-      ],),
-    );
+        itemCount: newData[0].length,
+        itemBuilder: (BuildContext context, int i) {
+          return Card(
+            elevation: 8,
+            margin: EdgeInsets.all(8.0),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+                padding: EdgeInsets.all(16),
+                child: Column(children: [
+                  Text(
+                    newData[1][i]["text"],
+                    textAlign: TextAlign.center,
+                  )
+                ])),
+          );
+        });
   }
 }
