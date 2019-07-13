645d80027952d57ea8f2f95a8cc1416d2ff0392d
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-07-13 14:29:55 -0500

    Removed unused imports and finished todo

--- lib/UI/Settings.dart
+++ lib/UI/Settings.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:scheduler/Constants.dart';
 import 'package:scheduler/Strings.dart';
 import 'package:scheduler/Tools/Storage.dart';
